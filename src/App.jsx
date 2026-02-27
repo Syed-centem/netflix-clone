edd145b621e99527a8d5037b30d2289cd764d8ca
Syed Junaid Hussain <syedjunaidhussain162@gmail.com>
2026-02-28 00:11:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,27 @@
 import React from 'react';
-import Nav from './components/Nav';
-import Banner from './components/Banner';
-import Row from './components/Row';
-import requests from './requests';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginScreen from './components/LoginScreen';
+import HomeScreen from './components/HomeScreen';
 
 function App() {
   return (
-    <div className="min-h-screen bg-black flex">
-      {/* Fixed Sidebar */}
-      <Nav />
-      
-      {/* Main Content Area - margin left to offset the fixed sidebar */}
-      <main className="flex-1 ml-20 overflow-x-hidden flex flex-col">
-        {/* Banner and Rows */}
-        <Banner />
-        <Row 
-          title="Trending Now" 
-          fetchUrl={requests.fetchTrending} 
-          isLargeRow 
-        />
-        <Row title="New This Week" fetchUrl={requests.fetchActionMovies} />
-        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-        
-        {/* Developer Credit Footer */}
+    <Router>
+      <div className="app">
+        <Routes>
+          {/* Default Route is the Login Screen */}
+          <Route path="/" element={<LoginScreen />} />
+          
+          {/* Main App Route */}
+          <Route path="/browse" element={<HomeScreen />} />
+        </Routes>
+      </div>
+      {/* Developer Credit Footer */}
         <footer className="w-full text-center py-8 mt-auto text-gray-500 text-sm">
           <p>
             Developed by <span className="text-red-600 font-semibold tracking-wider">Syed Junaid Hussain</span>
           </p>
         </footer>
-      </main>
-    </div>
+    </Router>
   );
 }
 
